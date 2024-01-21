9fc7c312ff158542a783a615476869dfcc8f23c9
imRkofficial <rkbagra0490@gmail.com>
2024-01-21 14:39:13 +0530

    added bcrypt js for password hashing and fix signup controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,24 +1,45 @@
 import User from './../models/user.model.js';
+import bcryptjs from 'bcryptjs';
 
 export const signUp = async (req,res)=>{
     const {username, email, password} = req.body;
      
     if(!email || !username || !password ){
-        res.status(401).json({
+       return res.status(401).json({
             message:"All fields are required"
         })
     }
 
+    const existedUser = await User.findOne({email});
+
+    if(existedUser){
+      return  res.status(500).json({
+            success:false,
+            message:"User already exists"
+        })
+    }
+
+    const hashPassword = bcryptjs.hashSync(password,10)
+
     const createdUser = new User ({
         username:username.toLowerCase(),
         email:email,
-        password:password
+        password:hashPassword
     });
 
+   try {
     const user = await createdUser.save();
 
-    res.status(201).json({
+  return  res.status(201).json({
         user,
-        message:"User created successfully"
+        message:"Signup Successfull"
     })
-} 
+   } catch (error) {
+    return  res.status(400).json({
+        message:error.message,
+        success:false
+    })
+   }
+} ;
+
+
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,4 +1,4 @@
-import  mongoose from 'mongoose'
+import  mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
     username:{
@@ -16,6 +16,8 @@ const userSchema = new mongoose.Schema({
         required:true
     }
 },{timestamps:true});
+
+
 
 const User = mongoose.model("User",userSchema);
 
