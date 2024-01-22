c98a9fd4990fb87dff9e8e14a9d16bb3497c23cd
imRkofficial <rkbagra0490@gmail.com>
2024-01-22 21:03:33 +0530

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,15 @@ import App from './App.jsx'
 import './index.css';
 import { store } from './redux/store.js';
 import { Provider } from 'react-redux';
+import {persistStor} from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <PersistGate persistor={persistStor}>
     <Provider store={store}>
     <App />
     </Provider>
+    </PersistGate>
   </React.StrictMode>,
 )
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './user/userSlice';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice";
+import { persistReducer,persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const config = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persestReducer = persistReducer(config, rootReducer);
 
 export const store = configureStore({
-    reducer:{
-        user:userReducer
-    }
+  reducer: persestReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
+
+
+export const persistStor = persistStore(store)
