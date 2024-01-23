b774100b8e22eab6723f915c5fec83457ee24ac4
imRkofficial <rkbagra0490@gmail.com>
2024-01-24 00:03:14 +0530

    complete dark mode functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,96 +1,101 @@
-import React from 'react';
-import {Avatar, Button, Dropdown, Navbar, TextInput} from 'flowbite-react';
-import {Link,useLocation} from 'react-router-dom';
-import {AiOutlineSearch} from 'react-icons/ai';
-import {FaMoon} from 'react-icons/fa';
-import { useSelector } from 'react-redux'
+import React from "react";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTheme } from "../redux/theme/themeSlice";
+
 
 const Header = () => {
   const path = useLocation().pathname;
-  const {currentUser} = useSelector(state => state.user);
+  const { currentUser } = useSelector((state) => state.user);
+  const { theme } = useSelector(state=> state.theme);
+  const dispatch = useDispatch();
 
   return (
     <>
-    <Navbar className='border-b-2'>
-      <Link to={"/"} className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-        <span className='py-1 px-2 bg-gradient-to-r from-indigo-500 to via-purple-500 to-pink-500 rounded-lg text-white'>Rahul's</span>Blog
-      </Link>
-      <form>
-        <TextInput
-          type='text'
-          placeholder='search'
-          rightIcon={AiOutlineSearch}
-          className='hidden lg:inline'
-        />
-      </form>
-      <Button className='h-10 w-12 lg:hidden ' pill color='gray'>
-        <AiOutlineSearch/>
-      </Button>
-      <div className='flex gap-2 md:order-2 '>
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-        <FaMoon/>
-        </Button>
-        {currentUser ? 
-        (
-          <Dropdown
-            arrowIcon={false}
-            inline
-            label={
-              <Avatar
-                alt='user avatar'
-                img={currentUser.profilePicture}
-                rounded
-              />
-            }
-          >
-            <Dropdown.Header>
-              <span className='block text-sm mt-2 mb-2'>@{currentUser.username}</span>
-              <span className='block text-sm font-medium truncate mt-3'>{currentUser.email}</span>
-            </Dropdown.Header>
-            <Link to={"/dashboard?tab=profile"}>
-              <Dropdown.Item>
-                Profile
-              </Dropdown.Item>
-            </Link>
-            <Dropdown.Divider />
-            <Dropdown.Item>
-              Sign Out
-            </Dropdown.Item>
-
-          </Dropdown>
-        ) 
-        :
-         (
-          <Link to={"/sign-in"}>
-          <Button gradientDuoTone={'purpleToBlue'} outline>
-            Sign In
-          </Button>
+      <Navbar className="border-b-2">
+        <Link
+          to={"/"}
+          className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        >
+          <span className="py-1 px-2 bg-gradient-to-r from-indigo-500 to via-purple-500 to-pink-500 rounded-lg text-white">
+            Rahul's
+          </span>
+          Blog
         </Link>
-         )
-         }
-       
-        <Navbar.Toggle/>
-      </div>
-      <Navbar.Collapse>
-          <Navbar.Link active={path === "/"} as={'div'}>
-            <Link to={'/'}>
-              Home
+        <form>
+          <TextInput
+            type="text"
+            placeholder="search"
+            rightIcon={AiOutlineSearch}
+            className="hidden lg:inline"
+          />
+        </form>
+        <Button className="h-10 w-12 lg:hidden " pill color="gray">
+          <AiOutlineSearch />
+        </Button>
+        <div className="flex gap-2 md:order-2 ">
+          <Button
+            className="w-12 h-10 hidden sm:inline"
+            color="gray"
+            pill
+            onClick={() => dispatch(toggleTheme())}
+          >
+            {theme === "light" ? <FaMoon /> : <FaSun/> }
+            
+          </Button>
+          {currentUser ? (
+            <Dropdown
+              arrowIcon={false}
+              inline
+              label={
+                <Avatar
+                  alt="user avatar"
+                  img={currentUser.profilePicture}
+                  rounded
+                />
+              }
+            >
+              <Dropdown.Header>
+                <span className="block text-sm mt-2 mb-2">
+                  @{currentUser.username}
+                </span>
+                <span className="block text-sm font-medium truncate mt-3">
+                  {currentUser.email}
+                </span>
+              </Dropdown.Header>
+              <Link to={"/dashboard?tab=profile"}>
+                <Dropdown.Item>Profile</Dropdown.Item>
+              </Link>
+              <Dropdown.Divider />
+              <Dropdown.Item>Sign Out</Dropdown.Item>
+            </Dropdown>
+          ) : (
+            <Link to={"/sign-in"}>
+              <Button gradientDuoTone={"purpleToBlue"} outline>
+                Sign In
+              </Button>
             </Link>
+          )}
+
+          <Navbar.Toggle />
+        </div>
+        <Navbar.Collapse>
+          <Navbar.Link active={path === "/"} as={"div"}>
+            <Link to={"/"}>Home</Link>
           </Navbar.Link>
-          <Navbar.Link active={path === '/about'} as={'div'}>
-            <Link to={'/about'}>
-              About
-            </Link>
+          <Navbar.Link active={path === "/about"} as={"div"}>
+            <Link to={"/about"}>About</Link>
           </Navbar.Link>
-          <Navbar.Link active={path === '/projects'} as={'div'}>
-            <Link to={'/projects'}>
-              Projects
-            </Link>
+          <Navbar.Link active={path === "/projects"} as={"div"}>
+            <Link to={"/projects"}>Projects</Link>
           </Navbar.Link>
         </Navbar.Collapse>
-     </Navbar>
+      </Navbar>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,13 +6,16 @@ import { store } from './redux/store.js';
 import { Provider } from 'react-redux';
 import {persistStor} from './redux/store.js';
 import { PersistGate } from 'redux-persist/integration/react';
+import ThemeProvider from './components/ThemeProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PersistGate persistor={persistStor}>
-    <Provider store={store}>
-    <App />
-    </Provider>
+     <Provider store={store}>
+      <ThemeProvider>
+       <App />
+      </ThemeProvider>
+     </Provider>
     </PersistGate>
   </React.StrictMode>
 )
