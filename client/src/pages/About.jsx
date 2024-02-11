b5ad89bbfa1a9915e9542245667f4b022c190acb
imRkofficial <rkbagra0490@gmail.com>
2024-02-11 13:58:27 +0530

    complete about and projects page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className='text-3xl font font-semibold text-center mb-3'>About Us</h1>
+          <h1 className='text-3xl font font-semibold text-center mb-3'>
+            Contact Us On: <a href='mailto:rkbagra0490@gmail.com' className='text-purple-500'>rkbagra0490@gmail.com</a>
+          </h1>
+           <div className="text-gray-500 font-medium ">
+            <p>Welcome to our blog, where we strive to provide valuable insights, tips, and resources on a wide range of topics to help you navigate the digital world with ease. Whether you're a seasoned professional looking to stay updated on the latest trends or a curious beginner eager to explore the vast realms of technology, our platform is designed to cater to all levels of expertise.</p>
+           </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import image from './../assets/movix.png';
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div className="flex flex-col justify-center max-w-2xl mx-auto p-3 gap-6 mt-2 mb-8">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">Build fun and engaging projects while learning MERN Stack</p>
 
-export default Projects
+      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="https://movies-kappa-eight.vercel.app/" target="_blank">
+          <img
+            className="rounded-t-lg"
+            src={image}
+            alt=""
+          />
+        </a>
+        <div className="p-5">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Movie Based Platform Using React JS
+            </h5>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          Movies are meant to be shared, discussed, and celebrated – and that's exactly what you'll find on Movie Detail. 
+          </p>
+          <a
+            href="https://movies-kappa-eight.vercel.app/" target="_blank"
+            className="font-bold inline-flex focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300  rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
+             Live Demo
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
